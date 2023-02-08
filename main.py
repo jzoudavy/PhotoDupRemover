eb9db7594928c061618eee4bb2c0f110ca69ea84
jzoudavy <jzoudavy@gmail.com>
2023-02-07 21:23:41 -0500

    vv

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
 import cv2
 import numpy as np
 import os
+import time
 
 
 # return list of pics and only pics. no videos
 def generateFileList(param):
-    a = [name for name in os.listdir(".") if (name.endswith(".jpeg") or name.endswith(".JPG") or name.endswith(".jpg"))  ]
-    print(len(a))
+    a = [name for name in os.listdir(r"C:\Users\jhg\Pictures\old_picture_dump") if (name.endswith(".jpeg") or name.endswith(".JPG") or name.endswith(".jpg"))  ]
+
     return a
 
 
 def main():
+    root_path=r"C:\Users\jhg\Pictures\old_picture_dump"
 
-    image_list=generateFileList(r"C:\Users\jhg\Pictures\old_picture_dump")
+    image_list=generateFileList(root_path)
 
     # Define a function to compute the Mean Squared Error between two images.
 
-    img1 = cv2.imread(r"C:\Users\jhg\Pictures\old_picture_dump\20160326_181531.jpg")
-    img2 = cv2.imread(r"C:\Users\jhg\Pictures\old_picture_dump\20160326_194640.jpg")
-    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-
-    # get dimensions of image
-    dimensions1 = img1.shape
-
-    # height, width, number of channels in image
-    height1 = img1.shape[0]
-    width1 = img1.shape[1]
-
-    # get dimensions of image
-    dimensions2 = img2.shape
-
-    # height, width, number of channels in image
-    height2 = img2.shape[0]
-    width2 = img2.shape[1]
-
-    if height2==height1 and width2 == width1:
-
-        h, w = img1.shape
-        diff = cv2.subtract(img1, img2)
-        err = np.sum(diff ** 2)
-        mse = err / (float(h * w))
-        print("Image matching Error between the two images:", mse)
-    else:
-        print("different dimensions skipped compare")
+    for image in image_list:
+        image_path1=os.path.join(root_path,image)
+        img1 = cv2.imread(image_path1)
+        for image in image_list:
+            image_path2 = os.path.join(root_path, image)
+            if image_path1 != image_path2:
+                img2 = cv2.imread(image_path2)
 
 
-# Press the green button in the gutter to run the script.
+                if img1.shape == img2.shape:
+                    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+                    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+
+
+
+                    h, w = img1.shape
+                    diff = cv2.subtract(img1, img2)
+                    err = np.sum(diff ** 2)
+                    mse = err / (float(h * w))
+                    print("Image matching Error between the two images:", mse)
+                    print("image 1:", image_path1)
+                    print("image 2:", image_path2)
+                #else we skip the compare
+
+'''
+possible ways to speed this thing up
+1. multithread it
+2. make it use the gpu
+3. run it on nvidia jetson nano, but network latency? connect nano to switch maybe?
+4. 
+'''
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    end_time = time.time()
+
+    print("time elapsed: {:.2f}s".format(time.time() - start_time))
