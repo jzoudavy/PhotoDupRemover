66556a8812a37df7e142d0e43dc6456a8ea33e50
jzoudavy <jzoudavy@gmail.com>
2023-02-08 23:27:49 -0500

    multi thread double for multi thread

--- main.py
+++ main.py
@@ -3,40 +3,40 @@ import numpy as np
 import os
 import threading
 import time
+import random
 
 class myThread (threading.Thread):
-   def __init__(self, threadID, name, *args):
+   def __init__(self, name, *args):
       threading.Thread.__init__(self)
-      self.threadID = threadID
+      self.threadID =  random.randrange(1000, 10000, 2)
       self.name = name
       self.args = args
    def run(self):
       print("Starting " + self.name)
-      compareImage(self.args[0],self.args[1],self.args[2],self.args[3])
+      compareImage(self.args[0], self.args[1])
       print("Exiting " + self.name)
 
 
 
     # Define a function to compute the Mean Squared Error between two images.
-def compareImage(img1, image_path1, image_list, root_path):
-    for image in image_list:
-        image_path2 = os.path.join(root_path, image)
-        if image_path1 != image_path2:
-            img2 = cv2.imread(image_path2)
+def compareImage(image_path1, image_path2):
+    img2 = cv2.imread(image_path2)
+    img1 = cv2.imread(image_path1)
 
-            if img1.shape == img2.shape:
-                img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-                img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    if img1.shape == img2.shape:
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
-                h, w = img1.shape
-                diff = cv2.subtract(img1, img2)
-                err = np.sum(diff ** 2)
-                mse = err / (float(h * w))
-                print("Image matching Error between the two images:", mse)
-                print("image 1:", image_path1)
-                print("image 2:", image_path2)
-        else:
-            print("Didn't compare.")
+        h, w = img1.shape
+        diff = cv2.subtract(img1, img2)
+        err = np.sum(diff ** 2)
+        mse = err / (float(h * w))
+
+        print("Image matching Error between the two images:", mse)
+        print("image 1:", image_path1)
+        print("image 2:", image_path2)
+    else:
+        print("Didn't compare.")
 
 # return list of pics and only pics. no videos
 def generateFileList(root_path):
@@ -52,15 +52,17 @@ def main():
     image_list=generateFileList(root_path)
 
 
-    for index,image in enumerate(image_list):
+    for index1,image in enumerate(image_list):
         image_path1=os.path.join(root_path,image)
-        img1 = cv2.imread(image_path1)
 
-        # Create new threads
-        thread1 = myThread(index, "Thread-"+str(index), img1, image_path1, image_list, root_path)
+        for index2, image in enumerate(image_list):
+            image_path2 = os.path.join(root_path, image)
+            if image_path1 != image_path2:
+                # Create new threads
+                thread1 = myThread("Thread-"+image, image_path1, image_path2)
 
-        # Start new Threads
-        thread1.start()
+                # Start new Threads
+                thread1.start()
 
 
 
@@ -68,6 +70,8 @@ def main():
 '''
 possible ways to speed this thing up
 1. multithread it
+1a: one for loop per thread execution time of 0.38s for 4 pics
+1b: two for loop per thread execution time is 0.02s for 4 pics
 2. make it use the gpu
 3. run it on nvidia jetson nano, but network latency? connect nano to switch maybe?
 4. 
